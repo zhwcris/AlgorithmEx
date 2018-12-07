a5b875ba9d9dd5c9267163e523ce58a6d34ea627
v02zk <313159314@qq.com>
2018-12-07 21:07:29 +0800

    removeBoxes

--- AlgoEx/src/RemoveBoxes.java
+++ AlgoEx/src/RemoveBoxes.java
@@ -5,16 +5,26 @@
  */
 public class RemoveBoxes {
     public int removeBoxes(int[] boxes) {
-
+        int n = boxes.length;
+        int[][][] dp = new int[n][n][n];
+        return dfs(boxes, 0, n-1, 0, dp);
     }
 
-    private int dfs(int[] boxed, int start, int end, int k, int len){
+    //dp[i][j][k] represents the max points from box[i] to box[j] with k boxes whose values equal to box[i]
+    private int dfs(int[] boxed, int i, int j, int k, int[][][] dp){
+        if(i > j)return 0;
+        if(dp[i][j][k] > 0)return dp[i][j][k];
 
-        int res = 0;
-        for (int i = start; i <= end; i++) {
-            int j = start;
-            while (j <= end && boxed[i] == boxed[j])j++;
-            
+        for (; i + 1 <= j && boxed[i] == boxed[i+1]; i++, k++);
+
+        int res = dfs(boxed, i+1, j, 0, dp) + (k + 1) * (k + 1);
+
+        for (int m = i+1; m <= j; m++){
+            if(boxed[m] == boxed[i]){
+                res = Math.max(res, dfs(boxed, i+1, m-1, 0, dp) + dfs(boxed, m, j, k+1, dp));
+            }
         }
+        dp[i][j][k] = res;
+        return res;
     }
 }
